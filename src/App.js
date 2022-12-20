470cb837aa1cfdaf5772f74df50b18b502b0bb69
yuncandoit <yun22490315@naver.com>
2022-12-20 14:03:13 +0900

    react-beginner_final

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/movie" element={<Detail />}></Route>
+        <Route path="/movie/:id" element={<Detail />}></Route>
         <Route path="/" element={<Home />}></Route>
       </Routes>
     </BrowserRouter>
